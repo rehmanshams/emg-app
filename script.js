a641380b4f62090333d730ac393252f39f9fc19f
rehmanshams <rehman.alphasquad@gmail.com>
2023-08-10 21:51:11 +0500

    fixing the selection

--- script.js
+++ script.js
@@ -994,3 +994,16 @@ function portViewValueAudioCunsumeSupport() {
 }
 
 window.addEventListener("scroll", portViewValueAudioCunsumeSupport);
+var tabs = document.getElementsByClassName('Tab');
+
+Array.prototype.forEach.call(tabs, function(tab) {
+    tab.addEventListener('click', setActiveClass);
+});
+
+function setActiveClass(evt) {
+    Array.prototype.forEach.call(tabs, function(tab) {
+        tab.classList.remove('active');
+    });
+    
+    evt.currentTarget.classList.add('active');
+}
